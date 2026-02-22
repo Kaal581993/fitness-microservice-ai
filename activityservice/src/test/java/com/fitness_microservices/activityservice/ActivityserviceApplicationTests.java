cd351b934ab3e2f8a448975a6f1e2b409a2fd6ab
Kaal581993 <viral.prajapati.1993@gmail.com>
2026-02-22 14:42:45 +0530

    Gateway push commits

--- activityservice/src/test/java/com/fitness_microservices/activityservice/ActivityserviceApplicationTests.java
+++ activityservice/src/test/java/com/fitness_microservices/activityservice/ActivityserviceApplicationTests.java
@@ -1,10 +1,44 @@
 package com.fitness_microservices.activityservice;
 
+import com.fitness_microservices.activityservice.model.Activity;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@SpringBootTest
+import static org.mockito.Mockito.mock;
+
+@SpringBootTest(properties = {
+    "spring.main.allow-bean-definition-overriding=true"
+})
+@ActiveProfiles("test")
 class ActivityserviceApplicationTests {
+
+	@TestConfiguration
+	static class MockConfig {
+		@Bean
+		@Primary
+		@SuppressWarnings("unchecked")
+		public KafkaTemplate<String, Activity> kafkaTemplate() {
+			return mock(KafkaTemplate.class);
+		}
+
+		@Bean
+		@Primary
+		public WebClient userServiceWebClient() {
+			return mock(WebClient.class);
+		}
+
+		@Bean
+		@Primary
+		public WebClient.Builder webClientBuilder() {
+			return WebClient.builder();
+		}
+	}
 
 	@Test
 	void contextLoads() {
